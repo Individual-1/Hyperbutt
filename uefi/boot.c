1f79840f54c91afd2d84d1543a7efd92e55a7d02
mpy <m@py>
2017-07-06 21:10:39 -0700

    Note to self, figure out why get doesnt work, maybe set doesnt work?

--- uefi/boot.c
+++ uefi/boot.c
@@ -4,10 +4,10 @@
 #include <efilib.h>
 #include <efiprot.h>
 
-//#include "info.h"
-//#include "util.h"
+#include "info.h"
+#include "util.h"
 
-//mem_map_t mem_map;
+mem_map_t mem_map;
 
 /*
  * EFI stub
@@ -20,7 +20,7 @@
  * - 
  *
  */
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
 {
     EFI_STATUS status;
     EFI_INPUT_KEY key;
@@ -31,19 +31,19 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     Print(L"test string\n");
 
     // populate memory map
-    //mem_map.memory_map = LibMemoryMap(&mem_map.num_entries, &mem_map.map_key, &mem_map.desc_size, &mem_map.desc_version);
+    mem_map.memory_map = LibMemoryMap(&mem_map.num_entries, &mem_map.map_key, &mem_map.desc_size, &mem_map.desc_version);
 
     // test code please ignore
     ST->ConIn->Reset(ST->ConIn, FALSE);
     while ((status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY) ;
 
-    /*
+    
     CHAR16 str[] = L"string";
     UINTN ts = sizeof(str);
 
     Print(L"Before set\n");
 
-    //efivar_set(L"test", &ts, str, FALSE);
+    efivar_set(L"test", &ts, str, FALSE);
 
     Print(L"After set\n");
 
@@ -55,9 +55,11 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     
     Print(L"Before get\n");
 
-    //efivar_get(L"test", &tsg, strg);
+    efivar_get(L"test", &tsg, strg);
 
+    Print(L"After get: results on next line\n");
     Print(strg);
+
 
     ST->ConIn->Reset(ST->ConIn, FALSE);
     while ((status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY) ;
@@ -67,7 +69,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     
     status = uefi_call_wrapper(RT->SetVirtualAddressMap, 4, mem_map.num_entries, mem_map.desc_size, mem_map.desc_version, mem_map.memory_map);
     //TODO error handling
-    */
+    
 
     return status;
 }
